fdb82923dc1c05d3fc9c8436768f794a370ec4b3
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-03 22:07:45 +0300

    done

---
+++ index.js
@@ -0,0 +1,54 @@
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
+
+// Напишите реализацию createTimerAnimator
+// который будет анимировать timerEl
+const createTimerAnimator = () => {
+    return (seconds) => {
+        timerEl.innerText = seconds;
+
+        const formatNumbers = (int) => String(int).padStart(2, "0");
+
+        const render = () => {
+            if (seconds === 0) {
+                clearInterval(Timer);
+            }
+
+            // const GMT = 3 * 60 * 60 * 1000;
+
+            const targetTime = new Date(seconds-- * 1000);
+
+            const elHours =
+                    +formatNumbers(parseInt(targetTime / 1000 / 60 / 60)) > 99
+                        ? 99
+                        : formatNumbers(
+                              parseInt(targetTime / 1000 / 60 / 60)
+                          ).slice(-2),
+                elMinutes = formatNumbers(targetTime.getMinutes()),
+                elSeconds = formatNumbers(targetTime.getSeconds());
+
+            timerEl.textContent = elHours + ":" + elMinutes + ":" + elSeconds;
+        };
+
+        const Timer = setInterval(render, 1000);
+
+        render();
+    };
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener("input", (event) => {
+    inputEl.value = inputEl.value.replace(/\D/g, "");
+});
+
+buttonEl.addEventListener("click", () => {
+    const seconds = Number(inputEl.value);
+
+    if (seconds === 0) return;
+
+    animateTimer(seconds);
+
+    inputEl.value = "";
+});
